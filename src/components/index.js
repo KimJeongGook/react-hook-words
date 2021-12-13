ac88d71e3fc3e3553af1e585fab745f0c5c86e12
KimJengGook <gooki1210@naver.com>
2021-12-13 10:56:21 +0900

    검색 구현

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Word } from './Word'
+export { default as Input } from './Input'
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,55 @@
-import React, { useState, useEffect} from "react";
-import { Word } from "../components";
+import React, { useState, useEffect } from "react";
+import { Word, Input } from "../components";
 
 const Home = () => {
     const [loading, setLoading] = useState(true)
     const [words, setWords] = useState([])
+    const [query, setQuery] = useState('') //검색 구현
 
-    useEffect( () => {
+    useEffect(() => {
         fetch('https://dictionary-search-words.herokuapp.com/api/words')
-        .then( res => res.json())
-        .then( result => {
-            const {words} = result
-            console.log(words)
-            setLoading(false)
-            setWords(words)
-        })
+            .then(res => res.json())
+            .then(result => {
+                const { words } = result
+                console.log(words)
+                setLoading(false)
+                setWords(words)
+            })
     }, [])
 
-    const homeUI = words.map(word => <Word key={word._id}
-                                            r_word={word.r_word} 
-                                            r_link={word.r_link}
-                                            r_seq={word.r_seq}
-                                            r_chi={word.r_chi}
-                                            r_pos={word.r_pos}
-                                            r_des={word.r_des}
-                                        />
+    const handleChange = (e) => {
+        const { value } = e.target
+        setQuery(value)
+    }
+
+    const homeUI = words
+        .filter(word => {
+            const r_word = word.r_word.toLowerCase()
+            const q = query.toLowerCase()
+            return r_word.includes(q)
+        })
+        .map(word => <Word
+            key={word._id}
+            r_word={word.r_word}
+            r_link={word.r_link}
+            r_seq={word.r_seq}
+            r_chi={word.r_chi}
+            r_pos={word.r_pos}
+            r_des={word.r_des}
+        />
         )
     return (
-        <div>
-            {loading? 'Loading...' : homeUI}
-        </div>
+        <>
+            {loading ? 'Loading...' : <div>
+                <Input name='search' type='text' placeholder="Search words ..."
+                    value={query} onChange={handleChange} />
+                <br /><br />
+                <div>검색 결과 : {query ? homeUI.length : '0'}개</div>
+                <hr />
+                <br />
+                <div>{query ? homeUI : '단어를 검색하세요'}</div>
+            </div>}
+        </>
     )
 }
 
